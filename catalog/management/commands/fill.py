438425fe17baee7cebfb13a89173731cfeaf5c4f
Marakushin Vadim <marakushin.vadim@gmail.com>
2024-04-20 19:51:11 +0300

    question 6

---
+++ catalog/management/commands/fill.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+
+    @staticmethod
+    def json_read_categories():
+        pass
+
+
+
+    @staticmethod
+    def json_read_products():
+        pass
+
+
+    def handle(self, *args, **options):
+        pass
