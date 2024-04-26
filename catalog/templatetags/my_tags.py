2a8ac123b2e56e12eb4c08ae4395144587e20465
Vadim <vadim.marakushin@gmail.com>
2024-04-26 13:00:02 +0300

    add data

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.filter()
 def media_filter(path):
     if path:
-        return f"media/{path}"
+        return (f"/media/{path}")
     return "#"
 
 
--- catalog/urls.py
+++ catalog/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from catalog.views import index, contact_info, base, product_detail
 
 urlpatterns = [
-    path("", index),
+    path("", index, name='index'),
     path("contact_info", contact_info),
     path('base', base),
-    path('product_detail/<int:pk>/', product_detail)
+    path('product_detail/<int:pk>/', product_detail, name='product_detail')
 ]
