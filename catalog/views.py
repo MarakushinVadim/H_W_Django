25120616b3095be6f3bf6233073570fb503a8494
Vadim <vadim.marakushin@gmail.com>
2024-05-07 12:31:41 +0300

    add data

--- catalog/views.py
+++ catalog/views.py
@@ -23,6 +23,10 @@ class ProductDetailView(DetailView):
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
 
 class BlogDetailView(DetailView):
     model = Blog
