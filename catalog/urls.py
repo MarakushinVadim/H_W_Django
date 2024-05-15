5ef196f96e804ac6c7796b9b062736c657fd7015
Vadim <vadim.marakushin@gmail.com>
2024-05-15 15:50:44 +0300

    add data

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,17 +1,20 @@
 from django.urls import path
 
 from catalog.views import ProductListView, ContactInfoView, BaseView, ProductDetailView, BlogCreateView, BlogListView, \
-    BlogDetailView, BlogUpdateView, BlogDeleteView
+    BlogDetailView, BlogUpdateView, BlogDeleteView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 urlpatterns = [
     path("", ProductListView.as_view(), name='index'),
     path("contact_info", ContactInfoView.as_view()),
     path('base', BaseView.as_view()),
     path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('catalog/create', BlogCreateView.as_view(), name='blogs_create'),
+    path('catalog/product_create', ProductCreateView.as_view(), name='product_create'),
+    path('catalog/<int:pk>/product_update', ProductUpdateView.as_view(), name='product_update'),
+    path('catalog/<int:pk>/product_delete', ProductDeleteView.as_view(), name='product_delete'),
+    path('catalog/blog_create', BlogCreateView.as_view(), name='blogs_create'),
     path('catalog/blog_list', BlogListView.as_view(), name='blog_list'),
     path('catalog/blog_detail/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
     path('catalog/<int:pk>/update', BlogUpdateView.as_view(), name='blog_update'),
-path('catalog/<int:pk>/delete', BlogDeleteView.as_view(), name='blog_delete')
+    path('catalog/<int:pk>/delete', BlogDeleteView.as_view(), name='blog_delete')
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -2,11 +2,29 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 from pytils.translit import slugify
 
+from catalog.forms import ProductForm
 from catalog.models import Product, Blog
 
 
 class ProductListView(ListView):
     model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:index')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:index')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:index')
 
 
 class ContactInfoView(TemplateView):
@@ -20,6 +38,7 @@ class BaseView(TemplateView):
 class ProductDetailView(DetailView):
     model = Product
 
+
 class BlogListView(ListView):
     model = Blog
 
@@ -27,6 +46,7 @@ class BlogListView(ListView):
         queryset = super().get_queryset(*args, **kwargs)
         queryset = queryset.filter(is_published=True)
         return queryset
+
 
 class BlogDetailView(DetailView):
     model = Blog
@@ -37,12 +57,13 @@ class BlogDetailView(DetailView):
         self.object.save()
         return self.object
 
+
 class BlogCreateView(CreateView):
     model = Blog
     fields = ('title', 'content', 'preview', 'create_at')
     success_url = reverse_lazy('catalog:blog_list')
 
-    def form_valid(selfl, form):
+    def form_valid(self, form):
         if form.is_valid():
             new_blog = form.save()
             new_blog.slug = slugify(new_blog.title)
@@ -50,10 +71,10 @@ class BlogCreateView(CreateView):
 
         return super().form_valid(form)
 
+
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'content', 'preview', 'create_at', 'is_published')
-
 
     def form_valid(selfl, form):
         if form.is_valid():
@@ -65,6 +86,7 @@ class BlogUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('catalog:blog_detail', args=[self.kwargs.get('pk')])
+
 
 class BlogDeleteView(DeleteView):
     model = Blog
